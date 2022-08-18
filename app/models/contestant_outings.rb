08f565c0f6e7efc44b122d979e5ed6fc01a161b1
Nikhil Shahi <shniks@gmail.com>
2022-08-18 10:38:11 -0700

    Created contestantoutings model. Created migration to create its table.

---
+++ app/models/contestant_outings.rb
@@ -0,0 +1,3 @@
+class ContestantOutings < ApplicationRecord
+
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_173044) do
+ActiveRecord::Schema.define(version: 2022_08_18_173713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2022_08_18_173044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+  end
+
+  create_table "contestant_outings", force: :cascade do |t|
+    t.bigint "contestant_id"
+    t.bigint "outing_id"
+    t.index ["contestant_id"], name: "index_contestant_outings_on_contestant_id"
+    t.index ["outing_id"], name: "index_contestant_outings_on_outing_id"
   end
 
   create_table "contestants", force: :cascade do |t|
@@ -41,5 +48,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_173044) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "contestant_outings", "contestants"
+  add_foreign_key "contestant_outings", "outings"
   add_foreign_key "contestants", "bachelorettes"
 end
