7e2fc4382d9ad7f1121c97645c924cc284048803
Nikhil Shahi <shniks@gmail.com>
2022-08-18 11:16:31 -0700

    Created show action and view for contestants.

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,7 +1,8 @@
 class ContestantsController < ApplicationController
 
   def show
-
+    @contestant = Contestant.find(params[:id])
+    @bach = Bachelorette.find(@contestant[:bachelorette_id])
   end
 
-end 
+end
