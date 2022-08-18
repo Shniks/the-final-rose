7d0671838cb3bb7c50b0fc9076c4cffc77022b38
Nikhil Shahi <shniks@gmail.com>
2022-08-18 11:19:07 -0700

    Created route for outings show, created outings controller, show action and show view.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/bachelorettes/:id', to: 'bachelorettes#show'
   get '/bachelorettes/:id/contestants', to: 'bachelorette_contestants#index'
   get '/contestants/:id', to: 'contestants#show'
+  get '/outings/:id', to: 'outings#show'
 end
