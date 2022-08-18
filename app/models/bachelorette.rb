1ad9f82ce40781eee7899e65f4097855f67732ff
Nikhil Shahi <shniks@gmail.com>
2022-08-18 12:13:35 -0700

    Created test and method to compute average age of contestants. Added average age in the view for bachelorette show page. Completed user story 5.

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -2,4 +2,8 @@ class Bachelorette <ApplicationRecord
   validates_presence_of :name, :season_number, :description
   has_many :contestants
 
+  def average_age
+    contestants.average(:age)
+  end 
+
 end
--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Bachelorette, type: :model do
     it { should validate_presence_of  :season_number }
     it { should validate_presence_of  :description }
   end
+
+  it 'class methods' do
+    bach = Bachelorette.create!(name: 'Amy Winehouse', season_number: 10, description: 'Wildest Season Ever!')
+
+    con_1 = bach.contestants.create!(name: 'Pete Buttigieg', age: 28, hometown: 'Los Angeles, CA')
+    con_2 = bach.contestants.create!(name: 'Adam Sandler', age: 25, hometown: 'St George, UT')
+    con_3 = bach.contestants.create!(name: 'Mike Tyson', age: 29, hometown: 'Las Vegas, NV')
+
+    expect(bach.average_age.round(1)).to eq(27.3)
+  end
 end
