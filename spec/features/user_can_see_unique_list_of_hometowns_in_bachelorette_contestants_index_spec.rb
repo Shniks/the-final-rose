1548f6dad15b292194ac449a139917558f2b9050
Nikhil Shahi <shniks@gmail.com>
2022-08-18 13:03:23 -0700

    Updated test with amended assertions.

--- spec/features/user_can_see_unique_list_of_hometowns_in_bachelorette_contestants_index_spec.rb
+++ spec/features/user_can_see_unique_list_of_hometowns_in_bachelorette_contestants_index_spec.rb
@@ -8,13 +8,13 @@ RSpec.describe 'When I visit a bachelorette contestants index page' do
 
     con_1 = bach.contestants.create!(name: 'Pete Buttigieg', age: 28, hometown: 'Los Angeles, CA')
     con_2 = bach.contestants.create!(name: 'Adam Sandler', age: 25, hometown: 'St George, UT')
-    con_3 = bach.contestants.create!(name: 'Mike Tyson', age: 29, hometown: 'Los Angeles, NV')
+    con_3 = bach.contestants.create!(name: 'Mike Tyson', age: 29, hometown: 'Los Angeles, CA')
     con_4 = bach_2.contestants.create!(name: 'Chris Pratt', age: 23, hometown: 'Denver, CO')
     con_5 = bach_2.contestants.create!(name: 'Trevor Noah', age: 35, hometown: 'Denver, CO')
 
     visit "/bachelorettes/#{bach.id}/contestants"
 
-    expect(page).to have_content("#{con_1.hometown} #{con_2.hometown}")
+    expect(page).to have_content("These Contestants are from these hometowns: #{con_1.hometown}, #{con_2.hometown}")
 
     visit "/bachelorettes/#{bach_2.id}/contestants"
 
